b812cd8d9101cc512c393e35193edac0e8bfbfdf
Alexander Derderian <alexander.derderian@dawsoncollege.qc.ca>
2024-11-01 08:44:28 -0400

    Update DayNightCycle.cs

--- Other/DayNightCycle.cs
+++ Other/DayNightCycle.cs
@@ -10,12 +10,12 @@ public class DayNightCycle : MonoBehaviour
     public TimeSpan currentTime;
     public Transform SunTransform;
     public Light Sun;
-    public Text timetext;
+    public Text timeText;
     public int days;
 
     public float intensity;
-    public Color fogday = Color.gray;
-    public Color fognight = Color.black;
+    public Color fogDay = Color.gray;
+    public Color fogNight = Color.black;
 
     public int speed;
 
@@ -35,7 +35,7 @@ public class DayNightCycle : MonoBehaviour
 
         currentTime = TimeSpan.FromSeconds(time);
         string[] temptime = currentTime.ToString().Split(":"[0]);
-        timetext.text = temptime[0] + ":" + temptime[1];
+        timeText.text = temptime[0] + ":" + temptime[1];
 
         SunTransform.rotation = Quaternion.Euler(new Vector3((time - 21600) / 86400 * 360, 0, 0));
         if (time > 43200)
@@ -43,7 +43,7 @@ public class DayNightCycle : MonoBehaviour
         else
             intensity = 1 - ((43200 - time) / 43200 * -1);
 
-        RenderSettings.fogColor = Color.Lerp(fognight, fogday, intensity * intensity);
+        RenderSettings.fogColor = Color.Lerp(fogNight, fogDay, intensity * intensity);
 
         Sun.intensity = intensity;
     }
